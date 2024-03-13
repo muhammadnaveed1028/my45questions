70d764494b7dec94fdf926f68268ae148948dc5b
muhammadnaveed1028 <163102918+muhammadnaveed1028@users.noreply.github.com>
2024-03-12 22:39:41 -0700

    Add files via upload
    Question  45/2

---
+++ main.js
@@ -0,0 +1,3 @@
+// Question # 2
+var personName = "Arbab";
+console.log("Hello", personName, "would you like to learn some python today?");
---
+++ main.ts
@@ -0,0 +1,3 @@
+// Question # 2
+let personName = "Arbab";
+console.log("Hello",personName, "would you like to learn some python today?");
